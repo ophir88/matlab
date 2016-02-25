38e7b6cb250032d217eaa7ea79eaa0022953628e
ophir abitbol <ophir@ophirs-MBP.home>
2016-02-25 23:03:46 +0200

    using gradient

--- pyramidFocus/depthForTripleFocus.m
+++ pyramidFocus/depthForTripleFocus.m
@@ -4,15 +4,17 @@ function [ depth ] = depthForTripleFocus( img1,img2 , img3)
 
 
 [r,c,d] = size(img1);
-kernelSize = max(r,c)/40;
+kernelSize = max(r,c)/90;
 kernelSize = round(kernelSize);
 H = fspecial('average',kernelSize);
 Hdepth = fspecial('average',round(kernelSize/5));
 
-imgGray1 = rgb2gray(img1);
-imgGray2 = rgb2gray(img2);
-imgGray3 = rgb2gray(img3);
-
+% imgGray1 = rgb2gray(img1);
+% imgGray2 = rgb2gray(img2);
+% imgGray3 = rgb2gray(img3);
+imgGray1 = imgradient( rgb2gray(img1));
+imgGray2 = imgradient(rgb2gray(img2));
+imgGray3 = imgradient(rgb2gray(img3));
 
 imgGray1 = imfilter(imgGray1,H,'replicate');
 imgGray2 = imfilter(imgGray2,H,'replicate');
@@ -21,96 +23,24 @@ imgs ={};
 imgs{1} = imgGray1;
 imgs{2} = imgGray2;
 imgs{3} = imgGray3;
-% depth = 3*imgs{1}./(imgs{3} + imgs{2} + imgs{1});
-% depth = normalize(depth);
-% depth = depth.^2;
-
-depth = zeros(r,c);
-idx1 = imgGray1 > imgGray2 & imgGray1 > imgGray3;
-idx2 = imgGray2 > imgGray1 & imgGray2 > imgGray3;
-idx3 = imgGray3 > imgGray1 & imgGray3 > imgGray2;
-depth(idx1) = 1;
-depth(idx2) = 0.5;
-depth(idx3) = 0;
-
-% depth = 1 - depth;
-% for i = 1 : r
-%     for j = 1 : c
-%         maxVal = 0;
-%         index = 0;
-%         for k = 1 : 3
-%             if (img1{k}(i,j)>maxVal)
-%                 maxVal = img1{k}(i,j);
-%                 index = k - 1;                
-%             end
-%         end
-%         depth(i,j) = index/2;
-%     end
-% end
-% depth = 1 - depth;
+depth = 3*imgs{1}./(imgs{3} + imgs{2} + imgs{1});
+depth = normalize(depth);
+meanVal = mean(mean(depth));
+depth = remapInterpolation(depth, (meanVal*10-5), 0.7);
 % depth = imfilter(depth,Hdepth,'replicate');
 
-% figure; imshow(depth);
-% depthGray = (imgGray3 - imgGray2 - imgGray1);
-% sorted = sort(reshape(depthGray,[],1), 'descend');
-% minVal = mean(sorted(round(length(sorted)*95/100):length(sorted)));
-% depthGray = (depthGray - minVal);
-% sorted = sort(reshape(depthGray,[],1), 'descend');
-% maxVal = mean(sorted(1:round(length(sorted)*5/100)));
-% depthGray = 1 - depthGray/ maxVal;
-% meanVal = mean(mean(depthGray));
-% depthGrayO = depthGray;
-% % depthGray(depthGray>1) = 1;
-% depthGray1 = depthGray.^1.5;
-% depthGray = remapInterpolation(depthGray, (meanVal*10-5), 0.7);
+% depth = depth.^2;
 
+% depth = zeros(r,c);
+% idx1 = imgGray1 > imgGray2 & imgGray1 > imgGray3;
+% idx2 = imgGray2 > imgGray1 & imgGray2 > imgGray3;
+% idx3 = imgGray3 > imgGray1 & imgGray3 > imgGray2;
+% depth(idx1) = 1;
+% depth(idx2) = 0.5;
+% depth(idx3) = 0;
 
-
-% depthGray(depthGray>1) = 1;
-% figure;
-% subplot(1,3,1);
-% imshow(depthGrayO);
-% subplot(1,3,2);
-% imshow(depthGray1);
-% subplot(1,3,3);
-% imshow(depthGray);
-
-
-
-% imgGray3 = imfilter(imgGray1,H2,'replicate');
-% imgGray4 = imfilter(imgGray2,H2,'replicate');
-% depthGray2 = (imgGray4 - imgGray3);
-% % depthGray2 = depthGray2/(max(max(depthGray2)));
-% 
-% sorted = sort(reshape(depthGray2,[],1), 'descend');
-% minVal = mean(sorted(round(length(sorted)*90/100):length(sorted)));
-% depthGray2 = (depthGray2 - minVal);
-% sorted = sort(reshape(depthGray2,[],1), 'descend');
-% maxVal = mean(sorted(1:round(length(sorted)*10/100)));
-% depthGray2 = 1 - depthGray2/ maxVal;
-% depthGray2(depthGray2>1) = 1;
-
-% figure;
-% subplot(1,3,1);
-% imshow(depthGray);
-% subplot(1,3,2);
-% imshow(depthGray2);
-% subplot(1,3,3);
-% imshow((depthGray2+depthGray)/2);
-
-% figure;
-% subplot(1,3,1);
-% imshow(depthGrad);
-% subplot(1,3,2);
-% imshow(depthGray);
-% subplot(1,3,3);
-% imshowpair(depthGrad,depthGray,'falsecolor');
-% if (gray == 1)
 depth = repmat(depth, [1,1,3]);
-% else
-% depth = repmat(depthGray2, [1,1,3]);
 
-% end
 
 end
 
--- pyramidFocus/pyramidFocus.m
+++ pyramidFocus/pyramidFocus.m
@@ -1,6 +1,6 @@
-img1 = im2double(imread('./photos/largeDiff/flower4/IMG_5952.jpg'));
-img2 = im2double(imread('./photos/largeDiff/flower4/IMG_5953.jpg'));
-img3 = im2double(imread('./photos/largeDiff/flower4/IMG_5954.jpg'));
+img1 = im2double(imread('./photos/largeDiff/flower1/IMG_5940.jpg'));
+img2 = im2double(imread('./photos/largeDiff/flower1/IMG_5941.jpg'));
+img3 = im2double(imread('./photos/largeDiff/flower1/IMG_5942.jpg'));
 
 img1 = imresize(img1,0.4);
 img2 = imresize(img2,0.4);
@@ -9,12 +9,6 @@ img3 = imresize(img3,0.4);
 [r,c,d] = size(img2);
 % close all;
 imgs = {};
-% figure; imshow(img1);
-% imwrite(img1,'mouseOriginal.jpg')
-
-% imgZeroPadded = zeros(size(img1));
-% imgZeroPadded(360:670,1:370,:) = img1(360:670,1:370,:);
-% figure; imshow(imgZeroPadded);
 
 imgs{1} = img1;
 imgs{2} = img2;
@@ -24,22 +18,6 @@ imgsTransformed = sift_estimate_transformation(imgs);
 % figure; imshow(img2);
 [img2, SUPPORT] = iat_inverse_warping(img2, imgsTransformed{2}.T, 'homography', 1:c, 1:r);
 [img3, SUPPORT] = iat_inverse_warping(img3, imgsTransformed{3}.T, 'homography', 1:c, 1:r);
-
-% figure;
-% imshowpair(WIMAGE,img1,'blend','Scaling','joint');
-
-% pyr1 = genPyr(img1,'laplace',5);
-% pyr2 = genPyr(WIMAGE,'laplace',5);
-% pyrFinal1 = {};
-% pyrFinal1{length(pyr1)} = pyr1{length(pyr1)};
-% [r,c,d] = size(pyr2{3});
-% for pyrNum = 1 : length(pyr1)-1
-% %     depthMap = depthForCoupleFocus(pyr1{pyrNum}, pyr2{pyrNum}, 0);
-% %     imgSmall = 0;
-%     pyrFinal1{pyrNum} = 0;
-%     
-% end
-% finalImg = pyrReconstruct(pyrFinal);
 
 pyr1 = genPyr(img1,'laplace',4);
 pyr2 = genPyr(img2,'laplace',4);
@@ -53,7 +31,7 @@ for pyrNum = 1 : length(pyr1)-1
     if (pyrNum < 2)
     depthMap1 = depthForCoupleFocus(pyr1{pyrNum}, pyr2{pyrNum}, 0);
     depthMap2 = depthForCoupleFocus(depthMap1, pyr3{pyrNum}, 0);
-    depthMap3 = depthForTripleFocus(pyr1{pyrNum}, pyr2{pyrNum}, pyr3{pyrNum});
+    depthMap3 = depthForTripleFocus(img1, img2, img3);
 
     figure;
     ax1=subplot(1,3,1);
@@ -62,13 +40,14 @@ for pyrNum = 1 : length(pyr1)-1
     imshow(depthMap2);
     ax3=subplot(1,3,3);
     imshow(depthMap3);
-% imshowpair(finalImg,finalImg2,'diff');
 
 linkaxes([ax1 ax2 ax3],'xy')
-    else
-        [r,c,d] = size(pyr2{pyrNum});
-        depthMap3 = imresize(depthMap3 , [r c]);
+%     else
+%         [r,c,d] = size(pyr2{pyrNum});
+%         depthMap3 = imresize(depthMap3 , [r c]);
     end
+     [r,c,d] = size(pyr2{pyrNum});
+        depthMap3 = imresize(depthMap3 , [r c]);
     depthMaps{pyrNum} = depthMap3;
     pyrFinal2{pyrNum} = real(pyr1{pyrNum}.*depthMap3);
     
