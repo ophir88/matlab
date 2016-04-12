d379c33f9b914741f33707bebaa34013f0f59d78
ophir abitbol <ophir@ophirs-MBP.home>
2016-04-12 23:47:40 +0300

    use of (S+H) / V

--- noiseEstimation/stdOfDarkAreas.m
+++ noiseEstimation/stdOfDarkAreas.m
@@ -2,7 +2,7 @@ function [ stds, histograms ] = stdOfDarkAreas( imgs )
 %STDOFDARKAREAS Summary of this function goes here
 %   Detailed explanation goes here
 
-stds = zeros(length(imgs),1);
+stds = zeros(length(imgs),4);
 % histograms = {};
 for i = 1 : length(imgs)
     img = imgs{i};
@@ -11,14 +11,30 @@ for i = 1 : length(imgs)
     imgSegmented = double(segmentImg(img) + 1)/10;
     imgSegmented = imresize(imgSegmented, [r c]);
     
-    indx = imgSegmented<0.6;
+    indx = imgSegmented<0.4;
+%     imgG = img;
+%     imgG = imgradient(rgb2gray(imgG));
+%     imgG(~indx)=0;
     imgHSV = rgb2hsv(img);
     imgH = imgHSV(:,:,1);
-    values = imgH(indx);
-%     h = histogram(values);
-%     histograms{i} = h;
-%     meanF = mean(values(:));
-    stds(i,1) = AjaNE(values,500);
+    imgS = imgHSV(:,:,2);
+    imgV = imgHSV(:,:,3);
+    
+    valuesH = imgH(indx);
+    valuesS = imgS(indx);
+    valuesV = imgV(indx);
+    
+    %     h = histogram(values);
+    %     histograms{i} = h;
+    %     meanF = mean(values(:));
+    stds(i,1) = var(valuesH);
+    stds(i,2) = var(valuesS);
+    stds(i,3) = var(valuesV);
+    stds(i,4) = (stds(i,1)+stds(i,2))/stds(i,3);
+    if(stds(i,4)>7)
+        's';
+    end
+
 end
 
 end
